ff3c39ac81f3d73b24d2eb52a4f375fd94780a61
ggalindo-2022147 <ggalindo-2022147@kinal.edu.gt>
2024-03-06 14:40:57 -0600

    Se agrego getInventario al controlador

--- src/product/product.controller.js
+++ src/product/product.controller.js
@@ -26,6 +26,39 @@ export const getProducts = async (req = request, res = response) => {
     });
 }
 
+export const getProductsInventory = async (req = request, res = response) => {
+    const query = { estado: true };
+    const query2 = { estado: false };
+    const admin = req.admin._id;
+    const products = await Product.find(query);
+    const total = await Product.countDocuments(query2);
+
+
+    console.log(req.admin.role);
+    const adminResult = await Admin.findById(admin);
+
+
+    if (!adminResult || adminResult.role !== "ADMIN_ROLE") {
+        return res.status(400).json({
+            msg: "The admin does not exist or is not authorized"
+        });
+    } else {
+        if (!products) {
+            return res.status(400).json({
+                msg: "There are no products"
+            });
+        } else {
+            res.status(200).json({
+                products,
+                msg: `Products disabled found ${total}`
+            });
+        }
+    }
+}
+
+
+
+
 export const getProductByNombre = async (req = request, res = response) => {
     const { nombre } = req.params;
 
@@ -44,8 +77,8 @@ export const getProductByNombre = async (req = request, res = response) => {
 }
 
 export const productsGet = async (req, res) => {
-    const products = await Product.find({estado: true});
-    const total = await Product.countDocuments({estado: true});
+    const products = await Product.find({ estado: true });
+    const total = await Product.countDocuments({ estado: true });
     res.status(200).json({
         msg: "ccc",
         total,
